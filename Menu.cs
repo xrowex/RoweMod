090fa9f9407ff3fd6065efa93cc4ad81f17ec134
rowe <xrowex@gmail.com>
2025-06-04 16:50:50 -0400

    Add Premium tab

--- Menu.cs
+++ Menu.cs
@@ -40,7 +40,7 @@ namespace rowemod
             Graphics,
             Marker,
             Dropper,
-            //Premium,
+            Premium,
         }
 
         // Menu Variables
@@ -271,8 +271,10 @@ namespace rowemod
                     case Tab.Dropper:
                         ObjectDropper.DrawDropperTab();
                         break;
-                    //case Tab.Premium:
-                    //    break;
+                    case Tab.Premium:
+                        GUILayout.Box("Premium Features", coloredBoxStyle, GUILayout.Height(coloredBoxStyle.fixedHeight), GUILayout.ExpandWidth(true));
+                        GUILayout.Label("Premium-only settings will appear here.", labelStyle);
+                        break;
                 }
             }
             catch (Exception ex)
@@ -357,6 +359,7 @@ namespace rowemod
                 ("Graphics", Tab.Graphics),
                 ("Marker", Tab.Marker),
                 ("Dropper", Tab.Dropper),
+                ("Premium", Tab.Premium),
             };
 
             // Draw tab buttons with precise Rect positioning
@@ -417,6 +420,9 @@ namespace rowemod
                     case Tab.Dropper:
                         ObjectDropper.ResetTab();
                         Log.Msg("Dropper Tab reset!");
+                        break;
+                    case Tab.Premium:
+                        Log.Msg("Premium Tab reset!");
                         break;
                 }
 
